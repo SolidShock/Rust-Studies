79417667c2bdffa1f510fd7f3c4371f6eec3c9a2
SolidShock <lopexsw@gmail.com>
2021-10-04 15:53:55 +0300

    01 Hello, world!
    Created, compiled and ran a sweet little Cargo Project.

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\Users\lopex\Documents\Unreal Projects\Rust-Studies\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+C:\Users\lopex\Documents\Unreal Projects\Rust-Studies\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\lopex\Documents\Unreal\ Projects\Rust-Studies\hello_cargo\target\debug\hello_cargo.exe: C:\Users\lopex\Documents\Unreal\ Projects\Rust-Studies\hello_cargo\src\main.rs
